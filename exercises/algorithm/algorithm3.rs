9409d9f33b5641a67a61b552911956aa10b5e906
Artoria302 <artoria302@163.com>
2024-10-14 21:44:18 +0800

    algorithm2,3

--- exercises/algorithm/algorithm3.rs
+++ exercises/algorithm/algorithm3.rs
@@ -1,12 +1,38 @@
 /*
-	sort
-	This problem requires you to implement a sorting algorithm
-	you can use bubble sorting, insertion sorting, heap sorting, etc.
+    sort
+    This problem requires you to implement a sorting algorithm
+    you can use bubble sorting, insertion sorting, heap sorting, etc.
 */
-// I AM NOT DONE
 
-fn sort<T>(array: &mut [T]){
-	//TODO
+fn partition<T: PartialOrd>(array: &mut [T], i: usize, j: usize) -> usize {
+    let mut n = i;
+    for k in i..j {
+        if array[k] < array[j] {
+            array.swap(n, k);
+            n += 1;
+        }
+    }
+    array.swap(n, j);
+    n
+}
+
+fn sort0<T: PartialOrd>(array: &mut [T], i: usize, j: usize) {
+    if i >= j {
+        return;
+    };
+    let n = partition(array, i, j);
+    if n > 0 {
+        sort0(array, i, n - 1);
+    }
+    sort0(array, n + 1, j);
+}
+
+fn sort<T: PartialOrd>(array: &mut [T]) {
+    //TODO
+    if array.is_empty() {
+        return;
+    }
+    sort0(array, 0, array.len() - 1);
 }
 #[cfg(test)]
 mod tests {
@@ -18,13 +44,13 @@ mod tests {
         sort(&mut vec);
         assert_eq!(vec, vec![19, 37, 46, 57, 64, 73, 75, 91]);
     }
-	#[test]
+    #[test]
     fn test_sort_2() {
         let mut vec = vec![1];
         sort(&mut vec);
         assert_eq!(vec, vec![1]);
     }
-	#[test]
+    #[test]
     fn test_sort_3() {
         let mut vec = vec![99, 88, 77, 66, 55, 44, 33, 22, 11];
         sort(&mut vec);
